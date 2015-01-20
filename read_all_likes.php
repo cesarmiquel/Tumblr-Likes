507141cfacf4f78044314b7f32d358450ed5c6af
Cesar Miquel <miquel@easytech.com.ar>
2015-01-20 00:32:46 -0300

    Show a little more information when filling DB

--- read_all_likes.php
+++ read_all_likes.php
@@ -1,11 +1,17 @@
 <?php
 
 # Change these to your API key and posts
-$api_key = '[api key]';
-$blog    = '[blog name]';
+$api_key = '[api_key]';
+$blog    = '[blog_name]';
 $max_num_posts = 1000;
 
+$total = 0;
 for($i = 0; $i < $max_num_posts; $i += 20) {
     $url = "http://tumblr-likes.appspot.com/process?blog_name=$blog.tumblr.com&api_key=$api_key&offset=$i";
-    file_get_contents($url);
+    $out = file_get_contents($url);
+    $result = json_decode($out);
+    printf( "Saved [%s] posts.\n", $result->posts_saved);
+    $total += $result->posts_saved;
 }
+
+print "\n\nSaved $total posts.\n";
